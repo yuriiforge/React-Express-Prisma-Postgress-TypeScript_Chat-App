41190ffed9eff6bc5d2c3d00626caf5039c647df
Yurii <yurii.stepaniuk.dev@gmail.com>
2025-08-09 11:46:59 +0300

    Create HashService

---
+++ backend/services/hash.service.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcryptjs';
+
+class HashService {
+  private readonly saltRounds: number;
+
+  constructor() {
+    this.saltRounds = parseInt(process.env.SALT_ROUNDS!);
+  }
+
+  public async compare(data: string, hashedData: string): Promise<boolean> {
+    return await bcrypt.compare(data, hashedData);
+  }
+
+  public async hash(data: string): Promise<string> {
+    return bcrypt.hash(data, this.saltRounds);
+  }
+}
+
+export const hashService = new HashService();
