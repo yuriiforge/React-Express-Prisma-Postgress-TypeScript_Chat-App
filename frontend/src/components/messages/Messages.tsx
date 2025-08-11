781ccbd9278d0eb1aaf1d30b13290e1294e99f1b
Yurii <yurii.stepaniuk.dev@gmail.com>
2025-08-11 16:36:56 +0300

    Add skeletons for messages

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,17 +1,24 @@
 import useGetMessages from '../../hooks/useGetMessages';
-import Spinner from '../Spinner';
+import MessageSkeleton from '../skeletons/MessageSkeleton';
 import Message from './Message';
 
 const Messages = () => {
   const { loading, messages } = useGetMessages();
 
-  if (loading) return <Spinner />;
-
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {messages.map((message) => (
-        <Message key={message.id} message={message} />
-      ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading &&
+        messages.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-white">
+          Send a message to start a conversation
+        </p>
+      )}
     </div>
   );
 };
