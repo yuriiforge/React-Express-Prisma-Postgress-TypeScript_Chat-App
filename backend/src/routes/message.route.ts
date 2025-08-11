2284b45241908e6496744da769601e2ee4cd4acc
Yurii <yurii.stepaniuk.dev@gmail.com>
2025-08-11 10:52:02 +0300

    Create AuthController and send message route

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import protectRoute from '../middleware/protectRoute';
+import { messageController } from '../controllers/message.controller';
+import { asyncHandler } from '../middleware/asyncHandler';
 
 const router = express.Router();
 
-router.get('/conversations', (req, res) => {
-  res.send('Conversation route');
-});
+router.post(
+  '/send/:id',
+  protectRoute,
+  asyncHandler(messageController.sendMessage)
+);
 
 export default router;
